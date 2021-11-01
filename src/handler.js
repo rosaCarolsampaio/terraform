552764d3ed9f985a7e3c66e1de71c9e774082646
rosasilva <rosasilva@ext.worten.pt>
2021-11-01 20:30:11 +0000

    Create function

--- src/handler.js
+++ src/handler.js
@@ -1,20 +1,5 @@
-'use strict';
-
-module.exports.hello = async function(event) {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(
-      {
-        message: 'Your function executed successfully!',
-        params: event.queryStringParameters,
-        secret: process.env.A_VARIABLE
-      },
-      null,
-      2
-    ),
-  };
-};
-
+exports.handler = async function (event, context) {
+  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
+  console.log("EVENT\n" + JSON.stringify(event, null, 2))
+  return context.logStreamName
+}
