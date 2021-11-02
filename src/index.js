2963ee4b2fc363d77ff5487722cfaf2ee1d942b0
rosasilva <rosasilva@ext.worten.pt>
2021-11-02 13:09:33 +0000

    invoke function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-exports.handler = async function (event, context) {
+export async function handler (event, context) {
   console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
   console.log("EVENT\n" + JSON.stringify(event, null, 2))
   return context.logStreamName
