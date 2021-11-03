817cf78b182fccbcfb616925bd335e78b925d108
rosasilva <rosasilva@ext.worten.pt>
2021-11-03 23:15:09 +0000

    exec by sh

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ exports.handler = async function(event, context) {
   console.log("EVENT\n" + JSON.stringify(event, null, 2))
   return context.logStreamName
 }
-
--- src/index.js
+++
@@ -1,5 +0,0 @@
-export async function handler (event, context) {
-  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
-  console.log("EVENT\n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
-}
