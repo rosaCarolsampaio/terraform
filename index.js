802e2445af3776dba1b254fb41aa60d9bacb69dd
rosasilva <rosasilva@ext.worten.pt>
2021-11-04 11:33:37 +0000

    clean

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-exports.handler = async function(event, context) {
-  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
-  console.log("EVENT\n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
-}
+// exports.handler = async function(event, context) {
+//   console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
+//   console.log("EVENT\n" + JSON.stringify(event, null, 2))
+//   return context.logStreamName
+// }
+
+module.exports.handler = async function(event) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(
+      {
+        message: 'Your function executed successfully!',
+        params: event.queryStringParameters,
+        secret: process.env.A_VARIABLE
+      },
+      null,
+      2
+    ),
+  };
+};
